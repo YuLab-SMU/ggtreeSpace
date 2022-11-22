7a5d1097e203647c4c416c536f551df360d385dc
William Lee <1016523468@qq.com>
2022-11-22 21:04:58 +0800

    phylospm modified
    Update parameters list and other improvement

--- R/ggphylospm.R
+++ R/ggphylospm.R
@@ -1,24 +1,33 @@
-#' Plot phylogenetic scatterplot matrix
+#' @title Plot phylogenetic scatterplot matrix
 #'
-#' @param tr 
-#' @param traits 
-#' @param title 
-#' @param xAxisLabels 
-#' @param yAxisLabels 
-#' @param tr.params 
-#' @param sptr.params 
+#' @param tr A phylogenetic tree
+#' @param traits A data frame containing 
+#' @param title Set the title for the phylogenetic scatterplot matrix
+#' @param xAxisLabels Set the lable of the x axis
+#' @param yAxisLabels Set the lable of the y axis
+#' @param tr.params List of parameters to customize the phylogenetic tree with continuous trait mapping as continuous colors on the branch
+#' @param sptr.params List of parameters to customize the phylomorphospaces
 #'
 #' @return
-#' @export
 #'
 #' @examples
-ggphylopsm <- function(tr, traits = NULL, title = "Phylogenetic Scatterplot Matrix", 
-                       xAxisLabels = NULL, yAxisLabels = NULL,
-                       tr.params = list(size = 1, colors = NULL),
-                       sptr.params = list(tippoint = TRUE, tiplab = FALSE, labdir = "horizon",
+#' library(ggtree)
+#' library(phytools)
+#' 
+#' tr <- rtree(10)
+#' a <- fastBM(tr, nsim = 4)
+#' 
+#' phylospm(tr, a)
+#' @export
+phylospm <- function(tr, traits = NULL, title = "Phylogenetic Scatterplot Matrix", 
+                     xAxisLabels = NULL, yAxisLabels = NULL,
+                     tr.params = list(size = 1, colors = NULL, panel.grid = TRUE),
+                     sptr.params = list(tippoint = TRUE, tiplab = FALSE, labdir = "horizonal",
                                           panel.grid = FALSE))
 {
-  cat("Preparing phylogenetic scatterplot matrix, please wait....\n")
+  options(warn = -1)
+  
+  cat("Preparing phylogenetic scatterplot matrix, please wait...\n")
   
   if(is.null(traits))
     stop("Traits data is required.")
@@ -26,11 +35,18 @@ ggphylopsm <- function(tr, traits = NULL, title = "Phylogenetic Scatterplot Matr
   if (is.null(tr.params$colors))
     tr.params$colors <- c("red", 'orange', 'green', 'cyan', 'blue')
   
-  ## default.tr.params <- 
+  default.tr.params <- list(size = 1, 
+                            colors = c("red", 'orange', 'green', 'cyan', 'blue'),
+                            panel.grid = TRUE)
+  new.tr.params <- set.params(tr.params, default.tr.params)
   
-  
-  ## default.sptr.params <- 
 
+  default.sptr.params <- list(tippoint = TRUE, 
+                              tiplab = FALSE, 
+                              labdir = "horizonal",
+                              panel.grid = FALSE)
+  new.sptr.params <- set.params(sptr.params, default.sptr.params)
+  
   
   nc <- ncol(traits)
   colnames(traits) <- c(paste("V", 1:nc, sep = ""))
@@ -45,38 +61,56 @@ ggphylopsm <- function(tr, traits = NULL, title = "Phylogenetic Scatterplot Matr
   for (i in 1:nc) for (j in 1:nc){
     n = n + 1
     if (i == j) {
-      plst[[n]] <- ggtree(ftrd, mapping = aes_string(color = paste("V", i, sep = "")), 
-                          continuous = "color", size = tr.params$size) + 
-                    theme(legend.position = "none",
-                          axis.line = element_line(color = "black")) +
-                    scale_color_gradientn(colors=tr.params$colors)
+      suppressMessages(p <- ggtree(ftrd, mapping = aes_string(color = paste("V", i, sep = "")), 
+                                   continuous = "color", size = new.tr.params$size) + 
+                              scale_color_gradientn(colors = new.tr.params$colors) +
+                              coord_cartesian(default = T) +
+                              theme_treeSpace())
+      
+      suppressMessages(p <- p + coordtrans(p, traits[,i]))
+      
+      if (new.tr.params$panel.grid){
+        p <- p + theme_bw()
+      }
+      plst[[n]] <- p
+      
       }
     else {
-      suppressMessages(p <- ggtreeSpace(tr, traits[,c(i, j)]) + coord_cartesian(default = T))
+      suppressMessages(p <- ggtreeSpace(tr, traits[,c(j, i)]) + 
+                              coord_cartesian(default = T))
       
-      if (sptr.params$tippoint){
-        p + geom_tippoint()
+      p <- lim_set(p, traits[,c(j, i)])
+      
+      if (new.sptr.params$tippoint){
+        p <- p + geom_tippoint()
       }
         
-      
-      if (sptr.params$tiplab){
-        if(sptr.params$labdir == "horizonal")
-          p + geom_tiplab(angle = 0)
-        if(sptr.params$labdir == "radial")
-          p + geom_tiplab()
+      if (new.sptr.params$panel.grid){
+        p <- p + theme_bw()
       }
       
-      if (sptr.params$panel.grid){
-        p + theme_bw()
+      if (new.sptr.params$tiplab){
+        if(new.sptr.params$labdir == "horizonal")
+            p <- p + geom_text_repel(mapping = aes(label = label), 
+                                     data = td_filter(is.tip = TRUE), angle = 0)
+        if(new.sptr.params$labdir == "radial")
+            p <- p + geom_text_repel(mapping = aes(label = label), 
+                                     data = td_filter(is.tip = TRUE))
       }
       
+
       plst[[n]] <- p
     }
   }
-    GGally::ggmatrix(plst,
-                     nc, nc,
-                     title = title, 
-                     xAxisLabels = xAxisLabels, 
-                     yAxisLabels = yAxisLabels,
-                     ) 
+    ### change spm method
+    spm <- GGally::ggmatrix(plst,
+                            nc, nc,
+                            title = title, 
+                            xAxisLabels = xAxisLabels, 
+                            yAxisLabels = yAxisLabels,
+                            ) 
+    
+    class(spm) <- c("phylospm", class(spm))
+    
+    spm
 }
--- R/utilities.R
+++ R/utilities.R
@@ -1,32 +1,49 @@
-### gp_trans
-#' Plot a phylomorphospace with a gm.prcomp object
-#'
-#' @param object 
-#' @param ... 
-#'
-#' @return
-#' @export
-#'
-#' @examples
-# gp_trans <- function(object, ...){
-#   
-# }
-
-
 
 #### coordination transition
-# coordtrans <- function(p, t){
-#   pd <- p$data
-#   pd$x <- pd$x - ((max(pd$x)-min(pd$x))/2)
-#   pd$y <- pd$y - ((max(pd$y)-min(pd$y))/2)
-#   pd$x <- pd$x*(((max(t) - min(t))/((max(pd$x)-min(pd$x)))))
-#   pd$y <- pd$y*(((max(t) - min(t))/((max(pd$y)-min(pd$y)))))
-#   pd$x <- pd$x - (min(pd$x)- min(t))
-#   pd$y <- pd$y - (min(pd$y) - min(t))
-#   p$data <- pd
-#   p <- p + 
-#     theme(aspect.ratio = 1) +
-#     xlim(min(pd$x) - 0.05*(max(pd$x)-min(pd$x)), max(pd$x) + 0.05*(max(pd$x)-min(pd$x))) +
-#     ylim(min(pd$y) - 0.05*(max(pd$y)-min(pd$y)), max(pd$y) + 0.05*(max(pd$y)-min(pd$y)))
-#   return(p)
-# }
+coordtrans <- function(p, t){
+  pd <- p$data
+  pd$x <- pd$x - ((max(pd$x)-min(pd$x))/2)
+  pd$y <- pd$y - ((max(pd$y)-min(pd$y))/2)
+  pd$x <- pd$x*(((max(t) - min(t))/((max(pd$x)-min(pd$x)))))
+  pd$y <- pd$y*(((max(t) - min(t))/((max(pd$y)-min(pd$y)))))
+  pd$x <- pd$x - (min(pd$x)- min(t))
+  pd$y <- pd$y - (min(pd$y) - min(t))
+  p$data <- pd
+  p <- p + 
+    xlim(min(pd$x) - 0.05*(max(pd$x)-min(pd$x)), max(pd$x) + 0.05*(max(pd$x)-min(pd$x))) +
+    ylim(min(pd$y) - 0.05*(max(pd$y)-min(pd$y)), max(pd$y) + 0.05*(max(pd$y)-min(pd$y)))
+  return(p)
+}
+
+
+
+
+
+
+lim_set <- function(p, df){
+  p <- p + xlim(min(df[,1]) - 0.05*(max(df[,1])-min(df[,1])), max(df[,1]) + 0.05*(max(df[,1])-min(df[,1]))) +
+           ylim(min(df[,2]) - 0.05*(max(df[,2])-min(df[,2])), max(df[,2]) + 0.05*(max(df[,2])-min(df[,2])))
+  
+  p
+}
+
+
+
+
+
+
+
+set.params <- function(input, default){
+  if (is.null(get_expr(input))) {
+    return(NULL)
+  }
+  intdi <- intersect(names(input), names(default))
+  setd <- setdiff(names(default), names(input))
+  seti <- setdiff(names(input), names(default))
+  intdi <- input[match(intdi, names(input))]
+  setd <- default[match(setd, names(default))]
+  seti <- input[match(seti, names(input))]
+  newp <- c(intdi, setd, seti)
+}
+
+
